cf17bf103bee8321e66b10896b631e43a5df859d
ChrstnCrrnd <74797306+ChrstnCrrnd@users.noreply.github.com>
2021-08-09 20:11:35 +0200

    updated gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, io};
+
 fn main() {
-    eprintln!("what")
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("Welcome to the 3n+1 calculating program!");
+    let starting_number: u128 = get_input("What number would you like to start at?").trim().parse().unwrap();
+
+    loop{
+        
+    }
+}
+
+fn is_odd(num: u128) -> bool{
+    if num % 2 == 0 {return false}
+    else{return true}
+}
+
+
+fn get_input(prompt: &str) -> String{
+    println!("{}",prompt);
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_goes_into_input_above) => {},
+        Err(_no_updates_is_fine) => {},
+    }
+    input.trim().to_string()
 }
